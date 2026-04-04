4c781f6c5557f30f847ef9c0d7ec29a1942915e7
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-04-04 16:39:58 +0700

    chore: home blade tailwind bug not solve

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,18 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Filkom Event</title>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Filkom Event</title>
 </head>
 <body>
-    <Section class="h-full w-[80%] mx-auto min-h-screen bg-tertiary flex justify-center items-center">
-        <h1 class="text-4xl">Unlock Your Passion through Competition and Become an Outstanding Student!</h1>
-    </Section>
+  <section class="h-full w-full min-h-screen bg-tertiary flex justify-center items-center">
+    <div class="w-[80%] mx-auto">
+      <h1 class="text-4xl font-bold">Unlock Your Passion through Competition and Become an Outstanding Student!</h1>
+      <p class="text-xl">We are committed to supporting every step of your journey toward victory in prestigious IT competitions, providing the mentorship and resources you need to excel. Don't wait register today, unleash your full potential, and show the world what you are truly capable of achieving!</p>
+    </div>
+  </section>
 </body>
 </html>
