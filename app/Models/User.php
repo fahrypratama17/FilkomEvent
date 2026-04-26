d8d36ef01272316ba0f9ff5a28541642fae22e44
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-04-26 08:28:19 +0700

    feat: create users table migration and update User model for PostgreSQL

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,20 +2,29 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'nim', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+    protected $primaryKey = 'user_id';
+    public $incrementing = true;
+    public $timestamps = false;
+
+    protected $fillable = [
+      'name',
+      'nim',
+      'email',
+      'password'
+    ];
+
+    protected $hidden = [
+      'password',
+    ];
 
     /**
      * Get the attributes that should be cast.
@@ -25,7 +34,6 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
