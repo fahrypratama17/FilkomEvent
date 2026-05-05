8faa8d6e572d2db969cbfb6fff30852bad9bdf2a
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-05-05 13:02:37 +0700

    feat: refactor user profile page and implement UserController for improved user data handling

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,41 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Service\MenuService;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index() {
-      $user = Auth::user();
+  public function index() {
+    $user = Auth::user();
 
-      return view('Mahasiswa.profile', [
-        'user' => $user,
-        'menuItems' => $this->getMenu(),
-        'settingItems' => $this->getSetting(),
-      ]);
-    }
-
-  private function getMenu() {
-    $role = auth()->user()->role;
-
-    if ($role === 'admin') {
-      return [
-        ['label' => 'Dashboard', 'route' => 'Admin.AdminDashboard', 'icon' => 'UserRound']
-      ];
-    }
-
-    return [
-      ['label' => 'Dashboard', 'route' => 'dashboard', 'icon' => 'House'],
-      ['label' => 'Bookmark', 'route' => 'bookmark', 'icon' => 'BookMarked'],
-      ['label' => 'History', 'route' => 'history', 'icon' => 'History'],
-      ['label' => 'List Event', 'route' => 'events.*', 'icon' => 'List'],
-    ];
-  }
-
-  private function getSetting() {
-    return [
-      ['label' => 'Profile', 'route' => 'profile', 'icon' => 'UserRound'],
-    ];
+    return view('Mahasiswa.profile', [
+      'user' => $user,
+      'menuItems' => MenuService::getMenu($user->role),
+      'settingItems' => MenuService::getSetting(),
+    ]);
   }
 }
