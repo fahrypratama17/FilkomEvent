d6f5de54a9fdb27c45ecf8d7cf9ebdf3c6a40089
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-05-27 12:24:50 +0700

    feat: update Vite configuration to include auth.css in build input

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [
     laravel({
-      input: ["resources/css/app.css", "resources/js/app.js"],
+      input: [
+        "resources/css/app.css",
+        "resources/css/auth.css",
+        "resources/js/app.js",
+      ],
       refresh: true,
     }),
     tailwindcss(),
@@ -13,7 +17,6 @@ export default defineConfig({
   server: {
     watch: {
       ignored: ["*/storage/framework/views/*"],
-
     },
   },
 });
