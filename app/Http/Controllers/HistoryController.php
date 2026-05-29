f2c6d85862fc6f8881cf5bceb5406fa9f04b44fe
AnizaHelwa <helwamahanani07@gmail.com>
2026-05-29 09:14:50 +0700

    fix(backend): bind event.certificate relation query and clean up redundant logic

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -16,6 +16,7 @@ class HistoryController extends Controller
 
     $query = Registration::with([
       'event.category',
+      'event.certificate'
     ])
       ->where('user_id', $user->user_id)
       ->orderBy('registration_date', 'desc');
@@ -36,7 +37,7 @@ class HistoryController extends Controller
 
     $categories = Category::all();
 
-    return view('Mahasiswa.history', [
+    return view('Mahasiswa.history-event', [
       'registrations' => $registrations,
       'categories' => $categories,
       'menuItems' => MenuService::getMenu($user->role),
